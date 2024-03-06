effaf9c5cc44109948e35088d0d3b1343290d3ed
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-06 22:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let elem = document.getElementById("status");
+// alert(parseInt(elem.innerHTML))
+function myFunc() {
+  elem.innerHTML = '<h1>Entered Metaverse</h1>';
+}
